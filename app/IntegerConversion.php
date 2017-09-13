25c5e480a9a8f4e6353d9cc1067f884f6ff59055
Ali <ma.essa.pk@gmail.com>
2017-09-13 14:19:38 +0100

    Add code to convert decimal values to roman numerals

--- app/IntegerConversion.php
+++ app/IntegerConversion.php
@@ -2,7 +2,42 @@
 
 namespace App;
 
-class IntegerConversion
+class IntegerConversion implements IntegerConversionInterface
 {
 
+    protected $romanToDecimalMap = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',        
+    
+    ];   
+
+    public function toRomanNumerals($integer) 
+    {
+        $romanNumeral = '';
+
+        if(isset($this->romanToDecimalMap[$integer])) {
+            return $this->romanToDecimalMap[$integer];
+        }
+
+        foreach($this->romanToDecimalMap as $decimal => $roman) {
+            if($decimal <= $integer) {
+                $romanNumeral .= str_repeat($roman, $integer/$decimal);
+                $integer = $integer % $decimal;
+
+            }
+        }
+
+        return $romanNumeral;
+    }
 }
